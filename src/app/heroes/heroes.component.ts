f57bfb55f8af0e684243bf0a26eaff1f5cc3c02b
Sina Keang <sinakeang@gmail.com>
2020-01-26 16:51:48 -0500

    Add mock heroes and displaying hero details.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HEROES} from '../mock-heroes';
 import {Hero} from '../hero';
 
 @Component({
@@ -7,14 +8,15 @@ import {Hero} from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
